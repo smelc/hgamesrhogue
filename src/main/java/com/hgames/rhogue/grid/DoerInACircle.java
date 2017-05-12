7813b43bce3b08db5fe53794c6f83f6c116809c1
smelc <hgamesdeveloper@gmail.com>
2017-05-12 14:26:27 +0200

    Add an abstract class to perform something on cells in a circle

---
+++ src/main/java/com/hgames/rhogue/grid/DoerInACircle.java
@@ -0,0 +1,31 @@
+package com.hgames.rhogue.grid;
+
+/**
+ * A class to execute some actions within all cells of a circle.
+ * 
+ * @author smelC
+ */
+public abstract class DoerInACircle {
+
+	/**
+	 * @param x
+	 *            The x-coordinate of the circle's center
+	 * @param y
+	 *            The y-coordinate of the circle's center
+	 * @param radius
+	 *            The circle's radius (rayon).
+	 */
+	public void doInACircle(int x, int y, int radius) {
+		if (radius < 0)
+			return;
+		for (int dx = -radius; dx <= radius; ++dx) {
+			final int high = (int) Math.floor(Math.sqrt((radius * radius) - (dx * dx)));
+			for (int dy = -high; dy <= high; ++dy) {
+				doOnACell(x + dx, y + dy);
+			}
+		}
+	}
+
+	protected abstract void doOnACell(int x, int y);
+
+}
