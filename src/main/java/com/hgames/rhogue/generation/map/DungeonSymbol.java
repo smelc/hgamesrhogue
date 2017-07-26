2112f9b5357babeed4db5f1125e3d801ffda65fe
smelc <hgamesdeveloper@gmail.com>
2017-07-26 21:27:51 +0200

    Add DungeonSymbol enum, preparing for dungeon generation

---
+++ src/main/java/com/hgames/rhogue/generation/map/DungeonSymbol.java
@@ -0,0 +1,29 @@
+package com.hgames.rhogue.generation.map;
+
+/**
+ * Possible symbols in {@link DungeonGenerator}.
+ * 
+ * @author smelC
+ */
+public enum DungeonSymbol {
+	/** A pit or chasm in brogue terms */
+	CHASM,
+	/** Dangerous water */
+	DEEP_WATER,
+	/** A door */
+	DOOR,
+	/** A floor */
+	FLOOR,
+	/** Grass through which the rogue sees */
+	GRASS,
+	/** Grass through which the rogue doesn't see */
+	HIGH_GRASS,
+	/** Water in which you can walk without any penaly */
+	SHALLOW_WATER,
+	/** The stair to the next level */
+	STAIR_DOWN,
+	/** The stair to the previous level */
+	STAIR_UP,
+	/** A wall */
+	WALL,
+}
