902584d05ba0bf2df2436d317d6954841ef811a1
smelc <hgamesdeveloper@gmail.com>
2017-07-26 08:46:52 +0200

    Add method 'tickTurn' to IEffect

--- src/main/java/com/hgames/rhogue/effect/IEffect.java
+++ src/main/java/com/hgames/rhogue/effect/IEffect.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import com.hgames.rhogue.animate.IAnimate;
 
+import squidpony.squidmath.RNG;
+
 /**
  * Some temporary or durable effects. They are usually attached to instances of
  * {@link IAnimate}.
@@ -11,6 +13,14 @@ import com.hgames.rhogue.animate.IAnimate;
  * @author smelC
  */
 public interface IEffect extends Serializable {
+
+	/**
+	 * Make one turn pass in {@code this}.
+	 * 
+	 * @param rng
+	 * @return Whether something changed.
+	 */
+	public boolean tickTurn(RNG rng);
 
 	/**
 	 * @return true if this effect has no effect anymore/should be removed from
