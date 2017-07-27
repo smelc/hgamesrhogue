55900795a1ed67951ebe9810d1fc741831aa989e
smelc <hgamesdeveloper@gmail.com>
2017-07-27 20:40:34 +0200

    Add SetZone and ZoneUnion that implement Zone

---
+++ src/main/java/com/hgames/rhogue/zone/SetZone.java
@@ -0,0 +1,61 @@
+package com.hgames.rhogue.zone;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import squidpony.squidgrid.zone.Zone;
+import squidpony.squidmath.Coord;
+
+/**
+ * A zone backed up by a {@link Set}. It's good if you call
+ * {@link Zone#contains(Coord)} a lot on it.
+ * 
+ * @author smelC
+ */
+public class SetZone extends Zone.Skeleton implements Zone {
+
+	protected final Set<Coord> set;
+
+	private static final long serialVersionUID = -4601216047166803945L;
+
+	/**
+	 * @param set
+	 *            The set to delegate to.
+	 */
+	public SetZone(Set<Coord> set) {
+		this.set = set;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return set.isEmpty();
+	}
+
+	@Override
+	public int size() {
+		return set.size();
+	}
+
+	@Override
+	public Iterator<Coord> iterator() {
+		return set.iterator();
+	}
+
+	@Override
+	public boolean contains(Coord c) {
+		return set.contains(c);
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		return contains(Coord.get(x, y));
+	}
+
+	@Override
+	public List<Coord> getAll() {
+		return new ArrayList<Coord>(set);
+	}
+
+}
---
+++ src/main/java/com/hgames/rhogue/zone/ZoneUnion.java
@@ -0,0 +1,81 @@
+package com.hgames.rhogue.zone;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import squidpony.squidgrid.zone.Zone;
+import squidpony.squidmath.Coord;
+
+/**
+ * A zone that is the union of two zones.
+ * 
+ * @author smelC
+ */
+public class ZoneUnion extends Zone.Skeleton {
+
+	protected final Zone z1;
+	protected final Zone z2;
+
+	protected transient List<Coord> all;
+	protected transient Coord center;
+
+	private static final long serialVersionUID = 1414500085497183858L;
+
+	/**
+	 * The union of two zones.
+	 * 
+	 * @param z1
+	 * @param z2
+	 */
+	public ZoneUnion(Zone z1, Zone z2) {
+		this.z1 = z1;
+		this.z2 = z2;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return z1.isEmpty() && z2.isEmpty();
+	}
+
+	@Override
+	public int size() {
+		return z1.size() + z2.size();
+	}
+
+	@Override
+	public boolean contains(int x, int y) {
+		return z1.contains(x, y) || z2.contains(x, y);
+	}
+
+	@Override
+	public boolean contains(Coord c) {
+		return z1.contains(c) || z2.contains(c);
+	}
+
+	@Override
+	public List<Coord> getAll() {
+		if (all == null) {
+			all = new ArrayList<Coord>(size());
+			all.addAll(z1.getAll());
+			all.addAll(z2.getAll());
+		}
+		return all;
+	}
+
+	@Override
+	public boolean intersectsWith(Zone other) {
+		return z1.intersectsWith(other) || z2.intersectsWith(other);
+	}
+
+	@Override
+	public Coord getCenter() {
+		if (center == null) {
+			final Coord z1c = z1.getCenter();
+			final Coord z2c = z2.getCenter();
+			center = Coord.get(Math.round(((z1c.x + z2c.x) * z1.size()) / 2f),
+					Math.round(((z1c.y + z2c.y) * z2.size()) / 2f));
+		}
+		return center;
+	}
+
+}
